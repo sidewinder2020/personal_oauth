ad48b2c39e2e0089f3be2d687f3b8cabbecf524a
sidewinder2020 <sdwinder1@gmail.com>
2017-10-10 15:24:23 -0600

    github login routes added

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  root 'home#index'
+
+  get '/auth/github', to: 'sessions#create', as: :github_login
+  get '/auth/github/callback', to: 'sessions#create'
+  delete '/logout', to:'sessions#destroy'
+
+end
